13c27aa9159921029ae4d59c8353b96784c1c3cb
KristinaBu <krisdinabui@gmail.com>
2023-11-14 12:33:06 +0300

    commit ->github ДЗ2 Переделана структура папок, как указано в ДЗ-2

--- app_tp/views.py
+++ app_tp/views.py
@@ -33,44 +33,44 @@ def paginate(objects, request, per_page=10):
 
 def index(request):
 
-    return render(request,'app_tp/index.html',
+    return render(request,'index.html',
                   {'questions' : paginate(QUETIONS, request, 5),
                    'tags' : TAGS})
 
 def hot(request):
     '''один вопрос под своим id'''
-    return render(request,'app_tp/index.html', {'questions' : paginate(HOT_QUETIONS, request, 5),
+    return render(request,'index.html', {'questions' : paginate(HOT_QUETIONS, request, 5),
                                                 'tags' : TAGS})
 
 def tag(request, tag_id):
     tag_item = TAGS[tag_id]
     question_item = QUETIONS[1]
-    return render(request,'app_tp/tag.html', {'tag' : tag_item, 'questions' : paginate(QUETIONS, request, 5),
+    return render(request,'tag.html', {'tag' : tag_item, 'questions' : paginate(QUETIONS, request, 5),
                                               'question' : question_item,  'tags' : TAGS})
 
 
 def answers(request):
-    return render(request, 'app_tp/question.html', {'answers' : AMSWERS,
+    return render(request, 'question.html', {'answers' : AMSWERS,
                    'tags' : TAGS})
 
 def question(request, question_id):
     question_item = QUETIONS[question_id]
-    return render(request,'app_tp/question.html', {'question' : question_item,
+    return render(request,'question.html', {'question' : question_item,
                                                    'tags' : TAGS,
                                                    'answers' : paginate(AMSWERS, request, 5)})
 
 
 
 def ask(request):
-    return render(request,'app_tp/ask.html', {'tags' : TAGS})
+    return render(request,'ask.html', {'tags' : TAGS})
 
 
 def login(request):
-    return render(request,'app_tp/login.html', {'tags' : TAGS})
+    return render(request,'login.html', {'tags' : TAGS})
 
 
 def signup(request):
-    return render(request,'app_tp/signup.html', {'tags' : TAGS})
+    return render(request,'signup.html', {'tags' : TAGS})
 
 
 
